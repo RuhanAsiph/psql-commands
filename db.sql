f7b6dd498c4aa57b18eb5aa821521d5bf4186781
Ruhan Asif <theruhaanasif@gmail.com>
2021-09-26 15:58:00 +0530

    first commit

---
+++ db.sql
@@ -0,0 +1,18 @@
+-- create alliph database 
+CREATE DATABASE alliph;
+-- list databases \l 
+-- list tables \d
+-- change database \c alliph
+
+-- create table 
+create table diary( 
+	id INT, 
+	name VARCHAR(20), 
+	thoughts VARCHAR(500)
+);
+
+-- insert into alliph 
+INSERT INTO diary (id, name, thoughts) values (1, 'yuhana', 'three messengers');
+
+-- views data in alliph 
+
